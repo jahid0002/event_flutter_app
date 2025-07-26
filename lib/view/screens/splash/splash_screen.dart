9974cab208045177a3f7daec6f35dd8bbfd7c511
jahid0002 <dev.jahid0009@gmail.com>
2025-07-26 15:02:45 +0600

    implement ci/cd

--- lib/view/screens/splash/splash_screen.dart
+++ lib/view/screens/splash/splash_screen.dart
@@ -2,29 +2,60 @@ import 'package:event_app/core/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SplashScreen extends StatefulWidget {
-  final Duration delay;
+// class SplashScreen extends StatefulWidget {
+//   final Duration delay;
 
-  const SplashScreen({super.key, this.delay = const Duration(seconds: 3)});
+//   const SplashScreen({super.key, this.delay = const Duration(seconds: 3)});
+
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     Future.delayed(widget.delay, () {
+//       if (mounted) {
+//         Get.offAllNamed(AppRoutes.selectedScreen);
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(body: Center(child: Text('Splash')));
+//   }
+// }
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  Timer? _timer;
+
   @override
   void initState() {
     super.initState();
-
-    Future.delayed(widget.delay, () {
-      if (mounted) {
-        Get.offAllNamed(AppRoutes.selectedScreen);
-      }
+    _timer = Timer(const Duration(seconds: 3), () {
+      Get.offAllNamed(AppRoutes.selectedScreen);
     });
   }
 
   @override
+  void dispose() {
+    _timer?.cancel(); // Cancel timer to prevent test failure
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Splash')));
+    return const Scaffold(body: Center(child: Text('Splash Screen')));
   }
 }
