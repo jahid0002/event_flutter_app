07e23f47e3800a03255a016f0a27b1f8d2a60126
jahid0002 <dev.jahid0009@gmail.com>
2025-07-28 09:56:05 +0600

    add logo in splash screen

--- lib/view/screens/splash/splash_screen.dart
+++ lib/view/screens/splash/splash_screen.dart
@@ -4,32 +4,6 @@ import 'package:event_app/view/components/custom_image/custom_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-// class SplashScreen extends StatefulWidget {
-//   final Duration delay;
-
-//   const SplashScreen({super.key, this.delay = const Duration(seconds: 3)});
-
-//   @override
-//   State<SplashScreen> createState() => _SplashScreenState();
-// }
-
-// class _SplashScreenState extends State<SplashScreen> {
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     Future.delayed(widget.delay, () {
-//       if (mounted) {
-//         Get.offAllNamed(AppRoutes.selectedScreen);
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold(body: Center(child: Text('Splash')));
-//   }
-// }
 import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
