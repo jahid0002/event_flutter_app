d782266588b27ad9fa68e8da1ba3199d7685b0f8
jahid0002 <dev.jahid0009@gmail.com>
2025-09-26 12:20:28 +0600

    set up multi langues system

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: deprecated_member_use
+// ignore_for_file: deprecated_member_use, unused_element
 
 import 'package:event_app/core/dependency/dependency_injection.dart';
 import 'package:event_app/core/routes/app_routes.dart';
@@ -13,42 +13,108 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'utils/app_colors/app_colors.dart';
 import 'view/components/device_utils/device_utils.dart';
+// Localization generated files
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// Lock Orientation
   DeviceUtils.lockDevicePortrait();
+
+  /// Dependency Injection
   DependencyInjection di = DependencyInjection();
   di.dependencies();
-  SocketApi.init();
-  // ...
 
+  /// Socket API Init
+  SocketApi.init();
+
+  /// Firebase Init
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  /// iOS Push Notification Permission
   requestIOSPermissions();
 
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale _locale = const Locale('en'); // Default English
+
+  void _changeLanguage(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      designSize: const Size(393, 852),
       minTextAdapt: true,
       splitScreenMode: true,
-      designSize: const Size(393, 852),
-      child: GetMaterialApp(
-        theme: CustomTheme.lightTheme,
-        debugShowCheckedModeBanner: false,
-        defaultTransition: Transition.fadeIn,
-        transitionDuration: const Duration(milliseconds: 200),
-        initialRoute: AppRoutes.splashScreen,
-        navigatorKey: Get.key,
-        getPages: AppRoutes.routes,
-      ),
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: "Multi Language App",
+          theme: CustomTheme.lightTheme,
+          debugShowCheckedModeBanner: false,
+          defaultTransition: Transition.fadeIn,
+          transitionDuration: const Duration(milliseconds: 200),
+          navigatorKey: Get.key,
+          initialRoute: AppRoutes.splashScreen,
+          getPages: AppRoutes.routes,
+
+          locale: _locale,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        );
+      },
     );
   }
 }
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   DeviceUtils.lockDevicePortrait();
+//   DependencyInjection di = DependencyInjection();
+//   di.dependencies();
+//   SocketApi.init();
+//   // ...
+
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+//   requestIOSPermissions();
+
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//       minTextAdapt: true,
+//       splitScreenMode: true,
+//       designSize: const Size(393, 852),
+//       child: GetMaterialApp(
+//         theme: CustomTheme.lightTheme,
+//         debugShowCheckedModeBanner: false,
+//         defaultTransition: Transition.fadeIn,
+//         transitionDuration: const Duration(milliseconds: 200),
+//         initialRoute: AppRoutes.splashScreen,
+//         navigatorKey: Get.key,
+//         getPages: AppRoutes.routes,
+//       ),
+//     );
+//   }
+// }
 
 class CustomTheme {
   static ThemeData get lightTheme {
